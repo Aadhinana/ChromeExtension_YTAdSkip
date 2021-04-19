2d9da460c909f62ac9ea13662c0569d6c6b51389
Aadhi Nana <ajibaa@gmail.com>
2021-04-19 15:24:48 +0530

    pushing it to git

---
+++ content.js
@@ -0,0 +1,17 @@
+console.log("chrome extension running!")
+
+const interval = setInterval(() => {
+  console.log("Interval setup");
+  closeYTAds();
+}, 1000);
+
+const closeYTAds = () => {
+  const buttonClasses = ["ytp-ad-skip-button", "ytp-ad-overlay-close-button"];
+  const buttonElements = buttonClasses.map(
+    (ele) => document.querySelector(`.${buttonClasses[0]}`) || null
+  );
+
+  buttonElements.forEach((ele) => {
+    if (ele) ele.click();
+  });
+};
